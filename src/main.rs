d81819622f13048799bebb1d988939c84f23b082
Petrichor <cotillion15@hotmail.com>
2022-04-26 20:20:03 -0700

    Set reasonable rate of greeting

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,17 @@ struct Name(String);
 
 pub struct HelloPlugin;
 
+struct GreetTimer {
+    timer: Timer,
+}
+
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
-            .add_system(greet_people);
+        app.insert_resource(GreetTimer {
+            timer: Timer::from_seconds(2.0, true),
+        })
+        .add_startup_system(add_people)
+        .add_system(greet_people);
     }
 }
 
@@ -47,8 +53,12 @@ fn add_people(mut commands: Commands) {
         .insert(Name("Sherlock".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    // update our timer with the time elapsed since the last update
+    // if that caused the timer to finish, we say hello to everyone
+    if timer.timer.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
     }
 }
